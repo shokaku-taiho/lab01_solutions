5105d0da292c4e4bb6814585d6d80446ebff1de8
shokaku-taiho <65055258+shokaku-taiho@users.noreply.github.com>
2020-05-19 21:48:07 -0400

    Update Q3_ArrayInsertionDemo.java

--- Q3_ArrayInsertionDemo.java
+++ Q3_ArrayInsertionDemo.java
@@ -1,10 +1,31 @@
-public class Q3_ArrayInsertionDemo {
+public class Q3_ArrayInsertionDemo{
 
-  public static int[] insertIntoArray(int[] beforeArray, int indexToInsert, int valueToInsert){
-    // Your code here
-  }
+	public static int[] insertIntoArray(int[] beforeArray, int indexToInsert, int valueToInsert){
+		int[] newArray;
+		newArray = new int[beforeArray.length +1];
+		for (int i=0;i<indexToInsert;i++) {
+			newArray[i] = beforeArray[i];
+		}
+		newArray[indexToInsert] = valueToInsert;
+		for (int i=indexToInsert; i<newArray.length-1; i++){
+			newArray[i+1] = beforeArray[i];
+		}
+		return newArray;
+	}
 
-  public static void main(String[] args){
-    // Your code here
-  }
+	public static void main(String[] args){
+		int[] arrayBefore=new int[] {1,5,4,7,9,6};
+		System.out.println("Array before insertion: ");
+		for (int i=0;i<arrayBefore.length;i++){
+			System.out.println(arrayBefore[i]);
+		}
+		int index=3;
+		int integer=15;
+		int[] array;
+		array=Q3_ArrayInsertionDemo.insertIntoArray(arrayBefore,index,integer);
+		System.out.println("Array after insertion of "+integer+" at position "+index+":");
+		for (int i=0;i<array.length;i++){
+			System.out.println(array[i]);
+		}
+	}
 }
