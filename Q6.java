0e7beda690bb081e229d4ef1c6785637bfa756f2
shokaku-taiho <65055258+shokaku-taiho@users.noreply.github.com>
2020-05-19 21:55:38 -0400

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -1,24 +1,61 @@
-public class Q6 {
-  public static void main(String[] args){
+import java.util.Arrays;
+import java.util.Scanner;
 
-    //your code here
+public class Q6{
+    public static void main(String[] args){
 
-  }
+        Scanner scanGrade=new Scanner(System.in);
+        double grade[]=new double[10];
 
-  public static double calculateAverage(double[] notes){
-    //your code here
-  }
+        for(int i=0; i<10; i++)
+        {
+            System.out.println("Please enter grade");
+            grade[i]=scanGrade.nextDouble();
+        }
+        System.out.println("Average is: "+calculateAverage(grade));
+        System.out.println("Median is: "+calculateMedian(grade));
+        System.out.println("Number Failed is: "+calculateNumberFailed(grade));
+        System.out.println("Number Passed is: "+calculateNumberPassed(grade));
+    }
 
-  public static double calculateMedian(double[] notes){
-    //your code here
-  }
+    public static double calculateAverage(double[] notes){
+        double average=0;
+        for(int i=0; i<notes.length; i++)
+        {
+            average+=notes[i];
+        }
+        average/=notes.length;
+        return average;
+    }
+    public static double calculateMedian(double[] notes){
+        Arrays.sort(notes);
+        int l=notes.length;
 
-  public static int calculateNumberFailed(double[] notes){
-    //your code here
-  }
-
-  public static int calculateNumberPassed(double[] notes){
-    //your code here
-  }
-
+        if(l%2==0)
+        {
+            return notes[l/2];
+        }
+        else
+        {
+            return (notes[(l-1)/2]+notes[(l/2)])/2;
+        }
+    }
+    public static int calculateNumberFailed(double[] notes){
+        int counter=0;
+        for(int i=0; i<notes.length; i++){
+            if(notes[i]<50.0){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public static int calculateNumberPassed(double[] notes){
+        int counter=0;
+        for(int i=0; i<notes.length; i++){
+            if(notes[i]>=50.0){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
